0c9ca04b1ce2d9d23755364d70718cab786e3c40
Mobapps167 <mobappssolutions167@gmail.com>
2023-03-04 16:25:11 -0500

    Home page work Done

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,52 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Home.css'
 
+const getData = () => {
+  let Data = localStorage.getItem('students');
+  if (Data) {
+    return JSON.parse(localStorage.getItem('students'));
+  } else {
+    return [];
+  }
+}
+
 const Home = () => {
-  const [sname, setname] = useState({
-    fname: "",
-    class: "",
-    phone: "",
-    address: ""
-  });
+  const [localStorageData, setLocalStorageData] = useState(getData());
+
+  const [register, setRegister] = useState({
+    fname: '',
+    class: '',
+    phone: '',
+    address: '',
+  })
 
   const inputEvent = (e => {
     const { value, name } = e.target
-    setname((preval) => {
+    setRegister((preval) => {
       return {
         ...preval,
-        [name]: [value],
+        [name]: value,
       };
     })
   })
 
-  const submit = () => {
-
+  const registerNow = () => {
+    setLocalStorageData([...localStorageData, register])
   }
+
+  useEffect(() => {
+    localStorage.setItem('students', [JSON.stringify(localStorageData)]);
+  }, [localStorageData])
 
   return (
     <>
-      <div>
-        <p>{sname.fname} {sname.class} {sname.address} {sname.phone}</p>
-      </div>
       <center><h2 className='center-color'>Register Now</h2></center><br />
       <div className='main-div'>
-        <div className='sub-div div1'>
-          <label for="full-name">Full Name</label><br />
-          <input type="text" id="full-name" name="fname" value={sname.fname} placeholder='Full Name' autoComplete='off' onChange={inputEvent} /><br /><br />
+        <div>
+          <div className='sub-div div1'>
+            <label htmlFor="full-name">Full Name</label><br />
+            <input type="text" id="full-name" name="fname" value={register.fname} placeholder='Full Name' autoComplete='off' onChange={inputEvent} /><br /><br />
+          </div>
+          <div className='sub-div div2'>
+            <label htmlFor="class">Class</label><br />
+            <input type="text" id="class" name="class" value={register.class} placeholder='Class' autoComplete='off' onChange={inputEvent} /><br /><br />
+          </div>
+          <div className='sub-div div3'>
+            <label htmlFor="phone">Phone</label><br />
+            <input type="number" id="phone" name="phone" value={register.phone} placeholder='91' autoComplete='off' onChange={inputEvent} /><br /><br />
+          </div>
+          <div className='sub-div div4'>
+            <label htmlFor="address">Address</label><br />
+            <textarea cols="136" rows="4" name="address" value={register.address} placeholder='Type your Address' id="address" autoComplete='off' onChange={inputEvent} />
+          </div>
+          <button onClick={registerNow}>Register Now</button>
         </div>
-        <div className='sub-div div2'>
-          <label for="class">Class</label><br />
-          <input type="text" id="class" name="class" value={sname.class} placeholder='Class' autoComplete='off' onChange={inputEvent} /><br /><br />
-        </div>
-        <div className='sub-div div3'>
-          <label for="phone">Phone</label><br />
-          <input type="number" id="phone" name="phone" value={sname.phone} placeholder='91' autoComplete='off' onChange={inputEvent} /><br /><br />
-        </div>
-        <div className='sub-div div4'>
-          <label for="address">Address</label><br />
-          <textarea cols="136" rows="4" name="address" value={sname.address} placeholder='Type your Address' id="address" autoComplete='off' onChange={inputEvent} />
-        </div>
-        <button type='button' value='submit' onClick={submit}>submit</button>
       </div>
     </>
   )
